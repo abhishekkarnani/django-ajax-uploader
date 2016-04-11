6a2a30b5bdd5cce4a53ffc5d8e4830a71f830a44
akarnani1 <abhishek.karnani224@gmail.com>
2016-04-12 03:02:12 +0530

    master

---
+++ ajaxuploader/backends/local.py
@@ -0,0 +1,58 @@
+from io import FileIO, BufferedWriter
+import os
+
+from django.conf import settings
+
+from ajaxuploader.backends.base import AbstractUploadBackend
+
+
+class LocalUploadBackend(AbstractUploadBackend):
+    UPLOAD_DIR = "uploads"
+
+    def setup(self, filename, *args, **kwargs):
+        self._path = os.path.join(
+            settings.MEDIA_ROOT, self.UPLOAD_DIR, filename)
+        try:
+            os.makedirs(os.path.realpath(os.path.dirname(self._path)))
+        except:
+            pass
+        self._dest = BufferedWriter(FileIO(self._path, "w"))
+
+    def upload_chunk(self, chunk, *args, **kwargs):
+        self._dest.write(chunk)
+
+    def upload_complete(self, request, filename, *args, **kwargs):
+        path = settings.MEDIA_URL + self.UPLOAD_DIR + "/" + filename
+        self._dest.close()
+        return {"path": path}
+
+    def update_filename(self, request, filename, *args, **kwargs):
+        """
+        Returns a new name for the file being uploaded.
+        Ensure file with name doesn't exist, and if it does,
+        create a unique filename to avoid overwriting
+        """
+        filename = os.path.basename(filename)
+        self._dir = os.path.join(
+            settings.MEDIA_ROOT, self.UPLOAD_DIR)
+        unique_filename = False
+        filename_suffix = 0
+
+        # Check if file at filename exists
+        if os.path.isfile(os.path.join(self._dir, filename)):
+            while not unique_filename:
+                try:
+                    if filename_suffix == 0:
+                        open(os.path.join(self._dir, filename))
+                    else:
+                        filename_no_extension, extension = os.path.splitext(filename)
+                        open(os.path.join(self._dir, filename_no_extension + str(filename_suffix) + extension))
+                    filename_suffix += 1
+                except IOError:
+                    unique_filename = True
+
+        if filename_suffix == 0:
+            return filename
+        else:
+            return filename_no_extension + str(filename_suffix) + extension
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+setup(
+    name = "ajaxuploader",
+    description = "AJAX file uploader for django",
+    author = "Steven Skoczen",
+    author_email = "steven@agoodcloud.com",
+    url = "https://github.com/GoodCloud/django-ajax-uploader",
+    version = "0.2.4.5",
+    packages = find_packages(),
+    include_package_data=True,
+    zip_safe=False, # because we're including media that Django needs
+)
